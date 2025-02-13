01dd0065207089c7f8dd7c43d98189cc00c1a5a3
Luis Eduardo <luixst1@gmail.com>
2025-02-13 17:06:27 -0300

    [feat] Implementando consulta de situação do cliente

---
+++ msavaliadorcredito/src/main/java/com/compass/msavaliadorcredito/model/SituacaoCliente.java
@@ -0,0 +1,12 @@
+package com.compass.msavaliadorcredito.model;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class SituacaoCliente {
+
+    private DadosCliente cliente;
+    private List<CartaoCliente> cartoes;
+}
---
+++ msavaliadorcredito/src/main/java/com/compass/msavaliadorcredito/service/AvaliadorCreditoService.java
@@ -0,0 +1,15 @@
+package com.compass.msavaliadorcredito.service;
+
+import org.springframework.stereotype.Service;
+
+import com.compass.msavaliadorcredito.model.SituacaoCliente;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AvaliadorCreditoService {
+
+    public SituacaoCliente obSituacaoCliente(String cpf) {
+    }
+}
